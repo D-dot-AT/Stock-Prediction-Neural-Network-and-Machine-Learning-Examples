c5aca1d6f68bd15cb1a61eb8bba97ba62fc5ef32
brianrisk <brian@geneffects.com>
2023-09-12 08:57:49 -0400

    adding predictions to LR

--- machine_learning/logistic_regression_example.py
+++ machine_learning/logistic_regression_example.py
@@ -1,47 +1,54 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-
-# Step 1: Data Preparation
-# Load the training data
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 from common import print_statistics
 
+# Step 1: Data Preparation
+# 1. Load the training data
 train_data = pd.read_csv('../example_data/train.csv', header=None)
+
+# 2. Separate data into X (features) and Y (labels)
 X_train = train_data.iloc[:, :-1]
 Y_train = train_data.iloc[:, -1]
 
-# Scaling the features
+# 3. Scale the features
 scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
+X_train = scaler.fit_transform(X_train)
 
-# Step 2: Model Creation
-# Create a logistic regression model with hyperparameter tuning
-model = LogisticRegression(random_state=0, class_weight='balanced', penalty='l1', solver='liblinear')
+# Step 2: Model Creation and Training
+# 1. Ensure the model's input features match the number of columns in X
+model = LogisticRegression(max_iter=1000)
 
-# Step 3: Training the Model
-# Train the model using the training data
-model.fit(X_train_scaled, Y_train)
+# 2. Train the model using the X and Y variables
+model.fit(X_train, Y_train)
 
-# Step 4: Testing the Model
-# Load the test data
+# Step 3: Model Testing
+# 1. Load and scale the test data
 test_data = pd.read_csv('../example_data/test.csv', header=None)
-X_test = test_data.iloc[:, :-1]
+X_test = scaler.transform(test_data.iloc[:, :-1])
 Y_test = test_data.iloc[:, -1]
 
-# Use the scaler to scale the feature data of test.csv
-X_test_scaled = scaler.transform(X_test)
+# 2. Get predictions
+Y_pred = model.predict(X_test)
 
-# Run the model on the scaled test data to get predictions
-Y_pred_prob = model.predict_proba(X_test_scaled)[:, 1]
-threshold = 0.6  # Adjust this value based on your analysis to maximize precision
-Y_pred = (Y_pred_prob >= threshold).astype(int)
+# 3. Obtain the confusion matrix variables
+TN, FP, FN, TP = confusion_matrix(Y_test, Y_pred).ravel()
 
-# Calculate metrics
-TP = sum((Y_test == 1) & (Y_pred == 1))
-FP = sum((Y_test == 0) & (Y_pred == 1))
-TN = sum((Y_test == 0) & (Y_pred == 0))
-FN = sum((Y_test == 1) & (Y_pred == 0))
-
+# Execute print_statistics
 print_statistics(tp=TP, fp=FP, tn=TN, fn=FN)
 
+# Step 4: Creating Predictions
+# 1. Load data from latest.csv
+latest_data = pd.read_csv('../example_data/latest.csv')
 
+# 2. Predict scores using the model and print the top 5 stock tickers along with their percentage scores
+stock_tickers = latest_data.iloc[:, 0]
+feature_vectors = latest_data.iloc[:, 1:]
+feature_vectors_scaled = scaler.transform(feature_vectors)
+scores = model.predict_proba(feature_vectors_scaled)[:, 1]
+
+# Getting the top 5 stock tickers along with their percentage scores
+top_5_indices = scores.argsort()[-5:][::-1]
+for idx in top_5_indices:
+    print(f'{stock_tickers[idx]}: {scores[idx] * 100:.2f}%')
